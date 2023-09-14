138c4c9a7577ff248848edc69135b2dadaa28bfa
Melaku Garcia <56188626+megarbon@users.noreply.github.com>
2023-09-14 19:16:14 +0200

    primeros avances

---
+++ projects/00-hello-world/src/assets/App.jsx
@@ -0,0 +1,12 @@
+export function App() {
+    return (
+        <article>
+            <header>
+                <img src="https://unavatar.io/twitter/kikobeats" alt="avatar de kikobeats" />
+            </header>
+            <header>
+                <img src="https://unavatar.io/twitter/spotify" alt="avatar de spotify" />
+            </header>
+        </article>
+    )
+}
