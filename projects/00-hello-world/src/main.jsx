6d2f263cbfad880318ae34aecf2927746db12c14
Melaku Garcia <56188626+megarbon@users.noreply.github.com>
2023-09-16 16:34:36 +0200

    final changes
    Termino el episodio 1 de como hacer el componente del boton de tw

--- projects/00-hello-world/src/main.jsx
+++ projects/00-hello-world/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App.jsx'
-import './index.css'
+import { App } from './assets/App.jsx'
+import './assets/App.css'
+import { TwitterFollowCard } from './assets/TwitterFollowCard.jsx'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
-  <App />
+ <App />
 )
